1cff9e352d2fca2e768bd12ebff0764a6aa2d1e7
Anya Tamara <anyatmr@gmail.com>
2021-07-21 15:20:51 +0700

    initial commit

---
+++ src/main/java/com/zwallet/zwalletapi/ZwalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.zwallet.zwalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zwallet/zwalletapi/ZwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwallet.zwalletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
