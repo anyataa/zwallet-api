8fde90083a53efc669402e5f6726b114b372bc6c
Evander Kristalino <christrevand@gmail.com>
2021-07-22 10:44:10 +0800

    friendship & phoneNumber

---
+++ src/main/java/com/zwallet/zwalletapi/Model/Entity/PhoneNumberEntity.java
@@ -0,0 +1,40 @@
+package com.zwallet.zwalletapi.Model.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "phone_number_table")
+@Data
+@NoArgsConstructor
+public class PhoneNumberEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+  private Integer phoneNumberId;
+
+  @Column(length = 50, nullable = false)
+  private String phoneNumber;
+
+  @Column()
+  private boolean isDeleted;
+
+  @Column()
+  private boolean isPrimary;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private UserDetailEntity user;
+
+  public PhoneNumberEntity(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+}
