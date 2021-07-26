f74cee41e98fcb63c5eedd8f25dd7badc99ae999
Anya Tamara <anyatmr@gmail.com>
2021-07-26 09:22:32 +0700

    initial commit

--- src/main/java/com/zwallet/zwalletapi/Model/Dto/AccountDto.java
+++ src/main/java/com/zwallet/zwalletapi/Model/Dto/AccountDto.java
@@ -2,10 +2,6 @@ package com.zwallet.zwalletapi.Model.Dto;
 
 import com.zwallet.zwalletapi.Model.Entity.AccountEntity;
 import com.zwallet.zwalletapi.Model.Entity.UserDetailEntity;
-import com.zwallet.zwalletapi.Repository.UserDetailRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
