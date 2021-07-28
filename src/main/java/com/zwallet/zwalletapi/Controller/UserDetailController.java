a62e523edc1c60b6fbe191f0e5277596af247a23
Anya Tamara <anyatmr@gmail.com>
2021-07-28 16:30:40 +0700

    las commit top up fixation

--- src/main/java/com/zwallet/zwalletapi/Controller/UserDetailController.java
+++ src/main/java/com/zwallet/zwalletapi/Controller/UserDetailController.java
@@ -28,6 +28,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "*")
 public class UserDetailController {
     @Autowired
     UserDetailRepository userDetailRepository;
