d0a4ed29ec889aa22befe6732e9b70fcb517f6b9
Anya Tamara <anyatmr@gmail.com>
2021-08-05 13:15:11 +0700

    fix repo in transaction

--- src/main/java/com/zwallet/zwalletapi/Repository/TransactionRepository.java
+++ src/main/java/com/zwallet/zwalletapi/Repository/TransactionRepository.java
@@ -20,7 +20,7 @@ public interface TransactionRepository extends JpaRepository<TransactionEntity, 
 
     List<TransactionEntity> findByToAccountId(AccountEntity accountId);
 
-    @Query(value = "SELECT * FROM transaction_table WHERE to_account_id = ?1 OR from_account_id = ?1  ORDER BY transaction_timestamp", nativeQuery = true)
+    @Query(value = "SELECT * FROM transaction_table WHERE to_account_id = ?1 OR from_account_id = ?1   ORDER BY transaction_id DESC LIMIT 10", nativeQuery = true)
     List<TransactionEntity> findAllTransactionByAccountId(AccountEntity accountId);
 
     @Query(value = "SELECT * FROM transaction_table  WHERE  to_account_id = ?1 AND transaction_timestamp BETWEEN curdate()-7 AND curdate()  OR from_account_id = ?1 AND transaction_timestamp BETWEEN curdate()-7 AND curdate()  ORDER BY transaction_timestamp ", nativeQuery = true)
@@ -32,10 +32,10 @@ public interface TransactionRepository extends JpaRepository<TransactionEntity, 
     @Query(value = "SELECT if(from_account_id = ?1 , from_account_balance , to_account_balance), transaction_timestamp FROM transaction_table WHERE transaction_timestamp BETWEEN curdate()-7 AND curdate()  AND  from_account_id = ?1 OR transaction_timestamp BETWEEN curdate()-7 AND curdate()  AND to_account_id = ?1 ", nativeQuery = true)
     List<?> findTransactionBalanceHistory(AccountEntity accountId);
 
-    @Query(value = "SELECT * FROM transaction_table WHERE from_account_id = ?1 AND transaction_timestamp = current_date() OR to_account_id = ?1 AND transaction_timestamp = current_date() ORDER BY transaction_timestamp DESC LIMIT 2", nativeQuery = true)
+    @Query(value = "SELECT * FROM transaction_table WHERE from_account_id = ?1 AND transaction_timestamp = current_date() OR to_account_id = ?1 AND transaction_timestamp = current_date() ORDER BY transaction_id DESC LIMIT 2", nativeQuery = true)
     List<TransactionEntity> findToday2Transaction(AccountEntity accountId);
 
-    @Query(value = "SELECT * FROM transaction_table WHERE from_account_id = ?1 AND transaction_timestamp BETWEEN curdate()-7 AND curdate() -1 OR to_account_id = ?1 AND transaction_timestamp BETWEEN curdate()-7 AND curdate() -1 ORDER BY transaction_timestamp DESC LIMIT 2", nativeQuery = true)
+    @Query(value = "SELECT * FROM transaction_table WHERE from_account_id = ?1 AND transaction_timestamp BETWEEN curdate()-7 AND curdate() -1 OR to_account_id = ?1 AND transaction_timestamp BETWEEN curdate()-7 AND curdate() -1 ORDER BY transaction_id DESC LIMIT 2", nativeQuery = true)
     List<TransactionEntity> findWeek2Transaction(AccountEntity accountId);
 
     @Query(value = "SELECT IF(from_account_id = ?1 , from_account_balance , to_account_balance) FROM transaction_table WHERE from_account_id = ?1 AND transaction_timestamp =curdate()- ?2 OR to_account_id = ?1 AND transaction_timestamp = curdate()- ?1 ORDER BY transaction_id DESC LIMIT 1", nativeQuery = true)
